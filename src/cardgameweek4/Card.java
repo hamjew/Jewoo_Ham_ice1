b37bd27a5d00a81e6fdb07db6866c428a216ee7b
hamjew <59941716+hamjew@users.noreply.github.com>
2020-01-29 19:01:07 -0500

    Update Card.java

--- src/cardgameweek4/Card.java
+++ src/cardgameweek4/Card.java
@@ -5,7 +5,9 @@ package cardgameweek4;
  * @author dancye, 2019
  */
 public class Card {
-    
+    	
+	// 01-29-2020
+	
         public enum Suit {HEARTS, CLUBS,SPADES,DIAMONDS};
         public enum Value{ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING};
         private final Suit suit;
